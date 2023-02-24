24991eeb9b801a2035d83f4e45461e3c84822363
Mikhail Lavrov <lavrov.fd@gmail.com>
2023-02-24 19:57:24 +0400

    all ready (+ PP)

--- src/js/script.js
+++ src/js/script.js
@@ -40,5 +40,30 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     });
 
+    const selectSingleList = document.querySelectorAll('.custom-select__select');
+
+    selectSingleList.forEach((selectSingle) => {
+      const selectSingle_title = selectSingle.querySelector('.custom-select__select-title');
+      const selectSingle_labels = selectSingle.querySelectorAll('.custom-select__label');
+
+      // Toggle menu
+      selectSingle_title.addEventListener('click', () => {
+        if ('active' === selectSingle.getAttribute('data-state')) {
+          selectSingle.setAttribute('data-state', '');
+        } else {
+          selectSingle.setAttribute('data-state', 'active');
+        }
+      });
+
+      // Close when click to option
+      for (let i = 0; i < selectSingle_labels.length; i++) {
+        selectSingle_labels[i].addEventListener('click', (evt) => {
+          selectSingle_title.textContent = evt.target.textContent;
+          selectSingle.setAttribute('data-state', '');
+        });
+      }
+    });
+
+
   });
 });
